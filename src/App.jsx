89bd362e7e446956669b5f4aaf6564104d42cb34
Michael <michael.schwake@t-online.de>
2023-10-16 15:51:33 +0200

    dark-grey-vector World-Cities dot size by city size onclick coordinate display

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,91 @@
 import React, { useRef, useEffect } from "react";
-import Bookmarks from '@arcgis/core/widgets/Bookmarks';
 import Expand from '@arcgis/core/widgets/Expand';
 import MapView from "@arcgis/core/views/MapView";
 import ArcGISMap from "@arcgis/core/Map";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer.js";
+import Popup from "@arcgis/core/widgets/Popup.js";
+import Symbol from "@arcgis/core/symbols/Symbol.js";
 
 import "./App.css";
 
 function App() {
 
   const mapDiv = useRef(null);
+  const locatorUrl = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
 
   useEffect(() => {
     if (mapDiv.current) {
 
+      var cityPopulation = {
+        type: "size",
+        field: "POP",
+        legendOptions:{
+          title: "Population"
+        },
+        stops:[
+          {value: 100000, size: "5"},
+          {value: 250000, size: "10"},
+          {value: 500000, size: "20"},
+          {value: 1000000, size: "30"},
+          {value: 10000000, size: "50"}
+        ]
+      };
+
+      const citiesRenderer = {
+        type: "simple", 
+        visualVariables: [cityPopulation ],
+        symbol: {
+          type: "simple-marker", 
+          size: 5,
+          color: [150, 0, 150],
+          outline: null
+        }
+        
+      };
       
 
       const fl = new FeatureLayer({
-        portalItem: {  // autocasts as esri/portal/PortalItem
+        portalItem: { 
           id: "6996f03a1b364dbab4008d99380370ed",
-          url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Cities/FeatureServer/"
-          
+          url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Cities/FeatureServer/" ,
+          renderer: citiesRenderer, 
         }
       });
+
+      
 
 
 
       const mymap = new ArcGISMap({
-        basemap: 'gray-vector'
+        basemap: 'dark-gray-vector'
       });
 
+      fl.renderer = citiesRenderer;
       mymap.add(fl);
 
       const view = new MapView({
         container: mapDiv.current,
         map: mymap,
-        center: [7.6261,51.9607],
-        zoom: 10
+        center: [7.6261,51.9607], //MS
+        zoom: 6,
+        popup: new Popup({
+          dockEnabled: true,
+          dockOptions: {
+            breakpoint: false,
+            position: "top-right"
+          }
+        })
       });
 
-      const bookmarks = new Bookmarks({
-        view,
-        // allows bookmarks to be added, edited, or deleted
-        editingEnabled: true
+      
+      view.on("click", (e) => {
+        const lat = Math.round(e.mapPoint.latitude * 1000) / 1000;
+        const lon = Math.round(e.mapPoint.longitude * 1000) / 1000;
+        
+        view.popupEnabled = false;
+        view.popup.title = lon + ", " + lat;
+        view.popup.open();
       });
-
-      const bkExpand = new Expand({
-        view,
-        content: bookmarks,
-        expanded: true
-      });
-
-      // Add the widget to the top-right corner of the view
-      view.ui.add(bkExpand, "top-right");
 
     }
   }, []);
